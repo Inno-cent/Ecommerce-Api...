0ff46f62741c94f44658c41561543ea153c0bb92
innocent <ekweozorinnocent@gmail.com>
2024-05-31 21:17:08 +0100

    router

---
+++ routes/home.route.js
@@ -0,0 +1,9 @@
+//Get request for the main home page
+const express = require('express');
+
+const homeRouter = express.Router();
+
+homeRouter.get('/', (req, res) =>{
+    res.sendFile(__dirname + "index");
+});
+
